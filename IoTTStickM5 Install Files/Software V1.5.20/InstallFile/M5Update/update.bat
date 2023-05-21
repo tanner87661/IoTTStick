4be94dcf435d8a2c3e380c9ef83299665ba8f036
Hans R. Tanner <hans@tanner-family.org>
2023-05-20 23:21:26 -0400

    Delete IoTTStickM5 Install Files/Software V1.5.20/InstallFile/M5Update directory

--- IoTTStickM5 Install Files/Software V1.5.20/InstallFile/M5Update/update.bat
+++
@@ -1,4 +0,0 @@
-esptool --chip esp32 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 LNFP_M5Stick.ino.bootloader.bin 0x10000 LNFP_M5Stick.ino.bin 0x8000 LNFP_M5Stick.ino.partitions.bin 
-esptool write_flash 0x00210000 LNFP_M5Stick.spiffs.bin
-
-
