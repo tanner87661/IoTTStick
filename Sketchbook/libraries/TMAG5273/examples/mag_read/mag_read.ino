99a9db9e296aef1a8b1c23046b918b372bf57a48
akash <akashrana3333@gmail.com>
2025-01-22 19:48:26 +0530

    Update mag_read.ino

--- Sketchbook/libraries/TMAG5273/examples/mag_read/mag_read.ino
+++ Sketchbook/libraries/TMAG5273/examples/mag_read/mag_read.ino
@@ -8,11 +8,10 @@ float sensorData[3] = {0,0,0};
 void setup() {
   Serial.begin(115200);
   delay(500);
-  Wire.begin();
+  Wire.begin(<sdaPin>, <sclPin>, 400000);
   Serial.println("Initializing TMAG5273 3 axis hall effect sensor");
 
-  TwoWire* wire = new TwoWire();
-  magSensor = new TMAG5273(wire);
+  magSensor = new TMAG5273(&Wire);
   if (!magSensor)
   {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -30,10 +29,10 @@ void loop() {
     sensorData[1] = magSensor->getFluxDensity(axis_Y);
     sensorData[2] = magSensor->getFluxDensity(axis_Z);
 
-    uint16_t angleData =  magSensor->getAngleData();
+    float angleData =  magSensor->getAngleData();
     char dispText[120];
-    sprintf(dispText, "%f %f %d\n", sensorData[1], sensorData[2], angleData);
+    sprintf(dispText, "%.2f %.2f %.2f %.2f \n", sensorData[0], sensorData[1], sensorData[2], angleData/360);
     Serial.print(dispText);
-    delay(10);
+    delay(50);
 
 }
